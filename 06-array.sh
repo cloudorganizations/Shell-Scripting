dbc892fda5118a041bd9461f4e0c1196c48df0d9
harishcloud524 <harishcloud524@gmail.com>
2024-01-14 01:47:07 +0530

    Publishing 2nd time

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS={Apple, Banana, Orange}
+
+echo "First value: ${FRUITS[0]}"
+
+echo "Second value: ${FRUITS[1]}"
+
+echo "Second value: ${FRUITS[2]}"
+
+echo "All fruits: ${FRUITS[@]}"
